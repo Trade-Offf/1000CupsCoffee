7860f4c306a9d752cdeb0b6e68266d92a95389aa
jia_lin.li <jia_lin.li@ceasia.cn>
2021-07-14 22:28:57 +0800

    add Menu apart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import Menu from "./components/Menu/Menu";
+import MenuItem from "./components/Menu/MenuItem";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0}>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link 2</MenuItem>
+          <MenuItem>cool link 3</MenuItem>
+        </Menu>
+
         <Button> 默认 </Button>
         <Button disabled> 不可触摸 </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
