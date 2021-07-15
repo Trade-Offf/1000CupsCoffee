a420aca23ef8de0937c5159ddcb174b8973938bf
jia_lin.li <jia_lin.li@ceasia.cn>
2021-07-15 23:24:43 +0800

    add useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,43 @@
-import React from "react";
-import Button, { ButtonType, ButtonSize } from "./components/Button/button";
-import Menu from "./components/Menu/Menu";
-import MenuItem from "./components/Menu/MenuItem";
+import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/Menu'
+import MenuItem from './components/Menu/MenuItem'  
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0}>
-          <MenuItem>cool link</MenuItem>
-          <MenuItem>cool link 2</MenuItem>
-          <MenuItem>cool link 3</MenuItem>
+        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}} mode="vertical">
+          <MenuItem index={0}>
+            cool link
+          </MenuItem>
+          <MenuItem index={1} disabled>
+            cool link 2
+          </MenuItem>        
+          <MenuItem index={2}>
+            cool link 3
+          </MenuItem>
         </Menu>
-
-        <Button> 默认 </Button>
-        <Button disabled> 不可触摸 </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
-          BigBtn
-        </Button>
-        <Button btnType={ButtonType.Danger}>DangerBtn</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
-          SmallBtn
-        </Button>
-
-        <Button
+        <Button className="custom"> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
           target="_blank"
-          btnType={ButtonType.Link}
-          href="http://www.baidu.com"
+          rel="noopener noreferrer"
         >
-          BaiDu.Link
-        </Button>
-        <Button disabled btnType={ButtonType.Link} href="http://www.baidu.com">
-          BaiDu.Link
-        </Button>
+          Learn React
+        </a>
       </header>
     </div>
   );
-};
+}
 
 export default App;
