b02fee09fb1a1fbb7b2e12695290b829e1e15454
jia_lin.li <jia_lin.li@ceasia.cn>
2021-07-21 22:36:06 +0800

    fixed bug

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render, RenderResult, fireEvent, cleanup, wait } from '@testing-library/react'
-import Menu, {MenuProps} from './menu'
-import MenuItem from './menuItem'
-import SubMenu from './subMenu'
+import Menu, {MenuProps} from './Menu'
+import MenuItem from './MenuItem'
+import SubMenu from './SubMenu'
 const testProps: MenuProps = {
   defaultIndex: '0',
   onSelect: jest.fn(),
