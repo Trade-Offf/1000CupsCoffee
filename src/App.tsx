0650df5e4e78558b44dca43d77ee4842fdb95b82
jia_lin.li <jia_lin.li@ceasia.cn>
2021-07-22 22:38:23 +0800

    add Icon part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import Menu from "./components/Menu/Menu";
 import MenuItem from "./components/Menu/MenuItem";
 import SubMenu from "./components/Menu/SubMenu";
+import Icon from "./components/Icon/icon";
+library.add(fas);
 const App: React.FC = () => {
   return (
     <div className="App">
